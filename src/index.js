61e1a4e995e0fb15bd0c7f431326ae1c13f86c2d
samir-27 <sumrasamir27@gmail.com>
2026-05-12 16:00:53 +0530

    util code added

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ dotenv.config(
     }
 );
 
-const app = express();
-
-
-
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("mongodb connection error: ", err);
+    })
 
 
 

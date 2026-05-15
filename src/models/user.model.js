5d82335d1f45456fc578d558ca71ed8286a5f6d7
samir-27 <sumrasamir27@gmail.com>
2026-05-15 11:01:57 +0530

    file upload configration added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,6 +48,7 @@ const userSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
 userSchema.pre('save', async function (next) {
     if(!this.isModified('password')) {
         return next();
---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.COUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) {
+      return null;
+    }
+    const response = await coundinary.uploader.upload(filePath, {
+      resource_type: 'auto',
+    })
+    console.log("File uploaded successfully", response.url);
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(FilePath);
+    return null;
+  }
+}
+
+export { uploadOnCloudinary } 

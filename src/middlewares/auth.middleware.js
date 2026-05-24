9edccc0b751801f782a2b64952a579729b966921
samir-27 <sumrasamir27@gmail.com>
2026-05-24 16:50:33 +0530

    auth middleware added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+
+export const varifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.headers("Authorization")?.replace("Bearer ", "");
+        if (!token) {
+            throw new ApiError(401, "Unauthorized: No token provided");
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decoded._id).select("-password -refreshToken")
+        if (!user) {
+            throw new ApiError(401, "invalid token: user not found");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Unauthorized: Invalid token");
+    }
+}
+
+); 
